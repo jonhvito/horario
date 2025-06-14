4b368a56cfc3f7959f08c17470d83abe0e97e8af
jonh_vito <victorjoao8817@gmail.com>
2025-06-14 14:50:23 -0300

    main

--- jest.setup.js
+++ jest.setup.js
@@ -31,7 +31,19 @@ class LocalStorageMock {
   }
 }
 
+// Mock do crypto
+const crypto = {
+  randomUUID: () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      const r = Math.random() * 16 | 0;
+      const v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+};
+
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 global.DOMException = DOMException;
-global.localStorage = new LocalStorageMock(); 
+global.localStorage = new LocalStorageMock();
+global.crypto = crypto; 
